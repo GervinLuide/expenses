8c759a27f8c4baa3ffb880555232470ba83a9943
Gervin Luide <gervin.luide@khk.ee>
2023-04-05 22:06:49 +0300

    Added surrounding components

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
-import ExpenseItem from "./components/ExpenseItem";
-
+import Expenses  from "./components/Expenses";
 
 function App() {
     const expenses = [
         {
             date: new Date(2023, 0, 10),
-            title: "New book",
+            title: 'New book',
             price: 30.99
         },
         {
             date: new Date(2023, 0, 10),
-            title: "New jeans",
+            title: 'New Jeans',
             price: 99.99
         }
     ]
 
-  return (
-    <div className="App">
-        <ExpenseItem expenseData={expenses[0]}></ExpenseItem>
-        <ExpenseItem expenseData={expenses[1]}></ExpenseItem>
-    </div>
-  );
-}
+    return (
+        <div className="App">
+            <Expenses expenseData={expenses[0]}></Expenses>
+            <Expenses expenseData={expenses[1]}></Expenses>
+        </div>
+    );
 
+}
 export default App;
