a95c9219387f764b54cba22bd78a14b14ae0b9a1
Gervin Luide <gervin.luide@khk.ee>
2023-04-17 19:23:05 +0300

    Component file organization in project

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 import Expenses  from "./components/Expenses";
 
-function App() {
+const App = () => {
     const expenses = [
         {
             date: new Date(2023, 0, 10),
