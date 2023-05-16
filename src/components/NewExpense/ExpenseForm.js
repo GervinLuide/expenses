2a6e2a7bc27f0b249401077e81e4618a7f14171e
Gervin Luide <gervin.luide@khk.ee>
2023-05-16 23:55:43 +0300

    Use of read input(changes)

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,25 +2,35 @@ import React, {useState} from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
-    const [enteredTitle, setEnteredTitle] = useState('')
-    const [enteredAmount, setEnteredAmount] = useState('')
-    const [enteredDate, setEnteredDate] = useState('')
+    //const [enteredTitle, setEnteredTitle] = useState('')
+    //const [enteredAmount, setEnteredAmount] = useState('')
+    //const [enteredDate, setEnteredDate] = useState('')
     const [userInput, setUserInput] = useState({
         enteredTitle: '',
         enteredAmount: '',
         enteredDate: ''
     })
     console.log(userInput)
+
     const titleChangeHandler = (event) => {
-        setEnteredTitle(event.target.value)
+        setUserInput({
+            ...userInput,
+            enteredTitle: event.target.value
+        })
     }
 
     const amountChangeHandler = (event) => {
-        setEnteredTitle(event.target.value)
+        setUserInput({
+            ...userInput,
+            enteredTitle: event.target.value
+        })
     }
 
     const dateChangeHandler = (event) => {
-        setEnteredTitle(event.target.value)
+        setUserInput({
+            ...userInput,
+            enteredDate: event.target.value
+        })
     }
 
     return(
